dd71a1661245e26a12a5585865b14a3090a34462
jankojjs <janko@jjsolutions.rs>
2021-09-10 00:24:31 +0200

    Adds stopwatch

---
+++ src/components/Stopwatch/Stopwatch.js
@@ -0,0 +1,29 @@
+import TimeContext from "../../store/time-context";
+import { useState, useEffect, useContext } from 'react';
+import StopwatchCard from '../StopwatchCard/StopwatchCard';
+
+function Stopwatch() {
+    const TimeCtx = useContext(TimeContext);
+    const [isTracking, setIsTracking] = useState(false);
+
+    useEffect(() => {
+        if(localStorage.getItem('active_task')!==null) {
+            setIsTracking(true);
+        }else {
+            setIsTracking(false)
+        }
+    }, [TimeCtx])
+
+    function pauseClick() {
+        TimeCtx.pauseCounting();
+        setIsTracking(false);
+    }
+
+    return (
+        <div>
+        { isTracking && <StopwatchCard onClick={pauseClick} taskId={localStorage.getItem('active_task')}/> }
+            </div>
+            )
+}
+
+export default Stopwatch;
--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -1,10 +1,12 @@
 import MainNavigation from "./MainNavigation";
 import classes from './Layout.module.css';
+import Stopwatch from '../components/Stopwatch/Stopwatch';
 
 function Layout(props) {
     return (
         <div className={classes.grid}>
             { props.userOnline && <MainNavigation /> }
+            { props.userOnline && <div className={classes.stopwatch}><Stopwatch /></div> }
             <div>{props.children}</div>
         </div>
     )

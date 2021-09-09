c7a1ed0f1d6630f1abf5a21485728e3d655045cc
jankojjs <janko@jjsolutions.rs>
2021-09-10 01:26:56 +0200

    Adds task deletion

--- src/components/PageDetailCard/PageDetailCard.js
+++ src/components/PageDetailCard/PageDetailCard.js
@@ -4,12 +4,25 @@ import { AiFillCheckCircle } from "react-icons/ai";
 import { useHistory } from 'react-router-dom';
 import { IoIosArrowDropleft } from "react-icons/io";
 import TimeTracking from '../TimeTracking/TimeTracking';
+import { MdDelete } from "react-icons/md";
+import Backdrop from '../Backdrop/Backdrop';
+import DeleteTaskModal from '../DeleteTaskModal/DeleteTaskModal';
 
 function PageDetailCard(props) {
     const [inheritedTask,setInheritedTask] = useState(props.taskObj);
     const history = useHistory();
+    const [deleteModal, setDeleteModal] = useState(false);
+
     function backBtnHandler() {
         history.push('/project/'+inheritedTask.project_id);
+    }
+
+    function deleteHandler() {
+        setDeleteModal(true);
+    }
+
+    function closeDeleteModal() {
+        setDeleteModal(false);
     }
 
     return (
@@ -20,6 +33,7 @@ function PageDetailCard(props) {
                         { inheritedTask.task_active ==='0' ? <span className={classes.checked}><AiFillCheckCircle size={22} color={'green'} /></span> : <span className={classes.notChecked}></span>}
                     </div>
                     <div className={`${inheritedTask.task_active ==='0' ? 'completed' : 'notcompleted' }`}>{inheritedTask.task_name}</div>
+                    <div onClick={deleteHandler} className={classes.delete}><MdDelete size={23} /></div>
                 </div>
                 <div onClick={backBtnHandler} className={classes.back}>
                     <span className={classes.backIcon}><IoIosArrowDropleft size={22}/></span>
@@ -36,6 +50,8 @@ function PageDetailCard(props) {
                 <div>Time tracking:</div>
                 <TimeTracking taskDetails={inheritedTask}/>
             </div>
+            { deleteModal && <Backdrop onCancel={closeDeleteModal}/> }
+            { deleteModal && <DeleteTaskModal projectId={inheritedTask.project_id} taskId={inheritedTask.task_id} onCancel={closeDeleteModal}/> }
         </div>
     )
 }

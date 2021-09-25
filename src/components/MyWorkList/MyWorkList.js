6d2b39005eee90b99cae154bd7ab64b1f9768b7b
jankojjs <janko@jjsolutions.rs>
2021-09-26 00:03:34 +0200

    Adds MyWorkList

---
+++ src/components/MyWorkList/MyWorkList.js
@@ -0,0 +1,47 @@
+import classes from './MyWorkList.module.css';
+import { useState, useEffect } from 'react';
+import TaskCard from '../TaskCard/TaskCard';
+
+function MyWorkList(props) {
+    const [fetchedData, setFetchedData] = useState();
+    const [state, setState] = useState(false);
+    const [noTasks, setNoTasks] = useState(true);
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    function fetchData() {
+        fetch(
+            'http://jjsolutions.rs/api/myworkapi.php?username='+localStorage.getItem("username")
+        ).then(response => {
+            return response.json();
+        }).then(data => {
+            if(!data.length) {
+                props.setCount(0);
+            } else {
+                setFetchedData(data);
+                props.setCount(data.length);
+                setState(true);
+                setNoTasks(false);
+            }
+        });
+    }
+
+    function funcTwo() {
+        console.log('');
+    }
+
+    return (
+        <div className={classes.wrap}>
+           {state && fetchedData.map((singleDataItem)=>{
+                return (
+                    <TaskCard key={singleDataItem.task_id} taskId={singleDataItem.task_id} name={singleDataItem.task_name} label={singleDataItem.task_label} active={singleDataItem.task_active} finishTask={funcTwo} unfinishTask={funcTwo} />
+                )
+            })}
+            { noTasks && <div className={classes.noTask}>Congradulations! <br/> You have completed all of your tasks.</div> }
+        </div>
+    )
+}
+
+export default MyWorkList;
--- src/pages/MyWork.js
+++ src/pages/MyWork.js
@@ -1,10 +1,12 @@
 import classes from './MyWork.module.css';
 import { useState } from 'react';
 import TimeWeek from '../components/TimeWeek/TimeWeek';
+import MyWorkList from '../components/MyWorkList/MyWorkList';
 
 function MyWorkPage() {
     const [tasksView, setTasksView] = useState(true);
     const [timetrackView, setTimetrackView] = useState(false);
+    const [mwtCounter, setMwtCounter] = useState(0);
 
     function openTrackView() {
         setTasksView(false);
@@ -16,17 +18,21 @@ function MyWorkPage() {
         setTimetrackView(false);
     }
 
+    function setmwtCounterHandler(passedVar){
+        setMwtCounter(passedVar);
+    }
+
     return (
         <div className={classes.cardWrap}>
             <div>
                 <h2 className={classes.pageHeadline}>My Work</h2>
             </div>
             <div className={classes.cardOne}>
-                <span onClick={openTasksView} style={{ borderBottom: tasksView ? '2px solid #7048e8' : '' }} className={classes.navItem}>Tasks(5)</span>
+                <span onClick={openTasksView} style={{ borderBottom: tasksView ? '2px solid #7048e8' : '' }} className={classes.navItem}>Tasks ({mwtCounter})</span>
                 <span onClick={openTrackView} style={{ borderBottom: timetrackView ? '2px solid #7048e8' : '' }} className={classes.navItem}>TimeTracker</span>
             </div>
             <div className={classes.card}>
-                { tasksView && <div>aaa</div> }
+                { tasksView && <MyWorkList setCount={setmwtCounterHandler}/> }
                 { timetrackView && <TimeWeek /> }
             </div>
         </div>

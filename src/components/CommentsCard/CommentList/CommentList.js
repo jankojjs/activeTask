6b81d8a11b0644b9211a5b8a805fa9499049d9dc
jankojjs <janko@jjsolutions.rs>
2021-09-28 23:00:28 +0200

    Adds singlecomment component

--- src/components/CommentsCard/CommentList/CommentList.js
+++ src/components/CommentsCard/CommentList/CommentList.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import classes from './CommentList.module.css';
 import { RiDeleteBin5Line, RiEdit2Line } from "react-icons/ri";
+import CommentSingle from '../CommentSingle/CommentSingle';
 
 function CommentList(props) {
     const [comments] = useState(props.comms);
@@ -10,24 +11,7 @@ function CommentList(props) {
             {
                 comments.map((singleComment)=>{
                     return (
-                        <div key={singleComment.cid} className={classes.singleCommentWrap}>
-                            <div className={classes.avatar}>
-                                <div className={classes.avatarText}>{singleComment.username.charAt(0)}</div>
-                            </div>
-                            <div className={classes.commentPart}>
-                                <div className={classes.commentTop}>
-                                    <div className={classes.text}>
-                                        <span className={classes.name}>Janko S.</span>
-                                        <span className={classes.date}>{singleComment.timestamp.slice(0, 16)}</span>
-                                    </div>
-                                    <div className={classes.controls}>
-                                        <span className={classes.editIcon}><RiEdit2Line /></span>
-                                        <span className={classes.deleteIcon}><RiDeleteBin5Line /></span>
-                                    </div>
-                                </div>
-                                <div className={classes.description} dangerouslySetInnerHTML={{ __html: singleComment.comment_desc }}></div>
-                            </div>
-                        </div>
+                        <CommentSingle singleComment={singleComment} key={singleComment.cid}/>
                     )
                 })
             }

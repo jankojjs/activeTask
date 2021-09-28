a7396e39f4443ce29bad77495ca50f617781be39
jankojjs <janko@jjsolutions.rs>
2021-09-29 00:17:02 +0200

    can only control own comments

--- src/components/CommentsCard/CommentSingle/CommentSingle.js
+++ src/components/CommentsCard/CommentSingle/CommentSingle.js
@@ -31,13 +31,16 @@ function CommentSingle(props) {
             <div className={classes.commentPart}>
                 <div className={classes.commentTop}>
                     <div className={classes.text}>
-                        <span className={classes.name}>Janko S.</span>
+                        <span className={classes.name}>{singleComment.firstname} {singleComment.lastname.charAt(0)}.</span>
                         <span className={classes.date}>{singleComment.timestamp.slice(0, 16)}</span>
                     </div>
+                    {
+                    localStorage.getItem('username')===singleComment.username &&
                     <div className={classes.controls}>
                         {/* <span className={classes.editIcon} onClick={editCommentHandler}><RiEdit2Line /></span> */}
                         <span className={classes.deleteIcon} onClick={openDeleteModalHandler}><RiDeleteBin5Line /></span>
                     </div>
+                    }
                 </div>
                 <div className={classes.description} dangerouslySetInnerHTML={{ __html: singleComment.comment_desc }}></div>
             </div>

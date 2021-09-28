340a9cb333ec3a0df4e854f3c74f88ae2e1a48c7
jankojjs <janko@jjsolutions.rs>
2021-09-28 02:52:01 +0200

    Adds comments

--- src/components/CommentsCard/CommentsCard.js
+++ src/components/CommentsCard/CommentsCard.js
@@ -1,22 +1,12 @@
 import classes from './CommentsCard.module.css';
-import { useState } from 'react';
 import NewCommentButton from './NewCommentButton/NewCommentButton';
 
 function CommentsCard() {
-    const [newCommentHandler, setNewCommentHandler] = useState(false);
-
-    function openNewCommentModal() {
-        setNewCommentHandler(true);
-    }
-
-    function closeNewCommentModal() {
-        setNewCommentHandler(false);
-    }
 
     return (
         <div className={classes.card}>
             <div className={classes.discussion}>Discussion</div>
-            { newCommentHandler ? <div onClick={closeNewCommentModal}>aaa</div> : <NewCommentButton onClick={openNewCommentModal}/> }
+            <NewCommentButton />
         </div>
     )
 }
--- src/components/CommentsCard/NewCommentButton/NewCommentButton.js
+++ src/components/CommentsCard/NewCommentButton/NewCommentButton.js
@@ -1,12 +1,73 @@
 import classes from './NewCommentButton.module.css';
+import { useState } from 'react';
+import { useLocation } from 'react-router'; 
+import Editor from "../../Editor/Editor";
+import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
 
-function NewCommentButton(props) {
+function NewCommentButton() {
+    const location = useLocation();
+
+    const [newCommentHandler, setNewCommentHandler] = useState(false);
+    const [editorState, setEditorState] = useState('');
+    const [taskId, setTaskId] = useState(location.pathname.replace('/task/',''));
+
+    function submitCommentHandler() {
+        setNewCommentHandler(false);
+        fetchNewComment();
+    }
+
+    function openNewCommentModal() {
+        setNewCommentHandler(true);
+    }
+
+    function closeNewCommentModal() {
+        setNewCommentHandler(false);
+    }
+    
+    function changeEditorState(newValue) {
+        setEditorState(newValue);
+    }
+
+    const url = 'https://jjsolutions.rs/api/addcommentapi.php';
+    const formData = new FormData();
+    formData.append('task_id', taskId);
+    formData.append('user_id', localStorage.getItem('user_id'));
+    formData.append('comment_desc', editorState);
+
+    function fetchNewComment() {
+        fetch(url, {
+            method: 'POST',
+            body: formData
+        })
+        .then(response=>response.json())
+        .then(data => {
+            if(data.length === undefined) {
+                alert('Could not submit your comment.')
+            }
+        })
+    }
 
     return (
-            <div className={classes.newCommentBtn} onClick={props.onClick}>
-                <span className={classes.avatar}><div className={classes.avatarText}>{localStorage.getItem('username').charAt(0)}</div></span>
-                <span className={classes.fakeInputWrap}>Write a comment...</span>
+        <div className={classes.grid}>
+            <div className={classes.avatar}>
+                <div className={classes.avatarText}>{localStorage.getItem('username').charAt(0)}</div>
             </div>
+            { newCommentHandler ? 
+            <div>
+                <div className={classes.edit}>
+                    <Editor defVal={''} onUpdate={changeEditorState}/>
+                </div>
+                <div className={classes.controls}>
+                    <button onClick={closeNewCommentModal} className='alt'>Cancel</button>
+                    <button onClick={submitCommentHandler} className='btn'>Send</button>
+                </div>
+            </div>
+                : 
+            <div className={classes.newCommentBtn} onClick={openNewCommentModal}>
+                <span className={classes.fakeInputWrap}>Write a comment...</span>
+            </div> 
+            }
+        </div>
     )
 }
 
